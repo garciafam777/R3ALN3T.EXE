ac28b781da8a0d09fc8ff240b2f3036b02d05ca8
garciafam777 <Richard.g.Garcianieves@gmail.com>
2026-06-27 20:35:37 -0500

    Remove orphaned Copilot Source/R3ALN3T scaffold

--- Source/R3ALN3T/Private/R3ALN3TGameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-#include "R3ALN3TGameModeBase.h"
-#include "R3ALN3TCharacter.h"
-
-AR3ALN3TGameModeBase::AR3ALN3TGameModeBase()
-{
-	// Set C++ character as default pawn
-	DefaultPawnClass = AR3ALN3TCharacter::StaticClass();
-}
